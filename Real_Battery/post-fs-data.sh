545067738f5e4e2ed129bfa36beeb5a638daed45
shadow3 <1394025007@qq.com>
2023-05-13 09:24:36 +0000

    modified:   Real_Battery/post-fs-data.sh

--- Real_Battery/post-fs-data.sh
+++ Real_Battery/post-fs-data.sh
@@ -4,6 +4,6 @@ chmod a+x "$MODDIR/real_batt"
 if [[ ! -d /sys/class/power_supply/bms ]]; then
 	# 堵塞启动直到init完成
 	# 防止卡开机，10秒后自动放弃
-	timeout 10 $MODDIR/real_batt --init
+	timeout 10 $MODDIR/real_batt --init_mount
 fi
 nohup $MODDIR/real_batt >/dev/null 2>&1 &
