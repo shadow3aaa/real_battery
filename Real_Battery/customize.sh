0d5e99b68e13877d5b3b097ba1e756b49cd5af2d
shadow3 <1394025007@qq.com>
2023-05-13 11:08:25 +0000

    modified:   Real_Battery/customize.sh modified:   Real_Battery/post-fs-data.sh modified:   src/core.rs

--- Real_Battery/customize.sh
+++ Real_Battery/customize.sh
@@ -1,4 +1,5 @@
 ui_print "让部分支持的设备显示更符合实际状态的电量百分比"
+pkill -9 real_batt
 chmod a+x "$MODPATH/real_batt"
 nohup "$MODPATH/real_batt" >/dev/null 2>&1 &
 if [[ ! -d /sys/class/power_supply/bms ]]; then
--- Real_Battery/post-fs-data.sh
+++ Real_Battery/post-fs-data.sh
@@ -1,13 +1,11 @@
 MODDIR=${0%/*}
-pkill -9 real_batt >/dev/null 2>&1
+pkill -9 real_batt
 chmod a+x "$MODDIR/real_batt"
 if [[ ! -d /sys/class/power_supply/bms ]]; then
     {
         while [ ! -f /sys/class/power_supply/battery/capacity ]; do
             sleep 1
         done
-    	# 堵塞启动直到init完成
-    	# 防止卡开机，10秒后自动放弃
     	$MODDIR/real_batt --init_mount
     	nohup $MODDIR/real_batt >/dev/null 2>&1 &
 	} &
